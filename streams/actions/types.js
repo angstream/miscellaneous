5d2fb7f315638dd61044bd20af0ee9c343e9253e
HastaLaVista <ekarimov@hotmail.com>
2021-07-23 13:54:44 -0400

    Create types.js

---
+++ streams/actions/types.js
@@ -0,0 +1,27 @@
+export const SIGN_IN = "SIGN_IN";
+export const SIGN_OUT = "SIGN_OUT";
+
+export const CREATE_STREAM = "CREATE_STREAM";
+export const FETCH_STREAMS = "FETCH_STREAMS";
+export const FETCH_STREAM = "FETCH_STREAM";
+export const DELETE_STREAM = "DELETE_STREAM";
+export const EDIT_STREAM = "EDIT_STREAM";
+
+export const CREATE_QUOTE = "CREATE_QUOTE";
+export const FETCH_USERS = "FETCH_USERS";
+export const FETCH_QUOTE = "FETCH_QUOTE";
+export const DELETE_QUOTE = "DELETE_QUOTE";
+export const EDIT_QUOTE = "EDIT_QUOTE";
+
+
+export const CREATE_TAB = "CREATE_TAB";
+export const FETCH_TABS = "FETCH_TABS";
+export const FETCH_TAB = "FETCH_TAB";
+export const DELETE_TAB = "DELETE_TAB";
+export const EDIT_TAB = "EDIT_TAB";
+
+export const CREATE_QUESTION = "CREATE_QUESTION";
+export const FETCH_QUESTIONS = "FETCH_QUESTIONS";
+export const FETCH_QUESTION = "FETCH_QUESTION";
+export const DELETE_QUESTION = "DELETE_QUESTION";
+export const EDIT_QUESTION = "EDIT_QUESTION";
