34c7603db23e40426907c1c30c4ffdc64c1c3b9e
HastaLaVista <ekarimov@hotmail.com>
2022-02-03 15:49:42 -0500

    Update str3.js

--- mfbjs2/str3.js
+++ mfbjs2/str3.js
@@ -24,5 +24,49 @@ function findSub(s, t) {
   return -1;
 }
 
+function findSubString(s, t) {
+  var no_of_chars = 256;
+  var len1 = s.length;
+  var tlen = t.length;
+  var hash_t = Array.from({ length: no_of_chars }, (_, i) => 0);
+  var hash_s = Array.from({ length: no_of_chars }, (_, i) => 0);
+
+  // Store occurrence of characters of pattern
+  for (var i = 0; i < tlen; i++) hash_t[t.charAt(i).charCodeAt(0)]++;
+  var start = 0,
+    start_index = -1,
+    min_len = Number.MAX_VALUE;
+
+  var count = 0;
+  for (var j = 0; j < len1; j++) {
+    // Count occurrence of characters
+    var code = s.charAt(j).charCodeAt(0);
+    hash_s[code]++;
+    if (hash_s[code] <= hash_t[code]) count++;
+    if (count == tlen) {
+      //   //try to minimize window
+      while (
+        hash_s[s.charAt(start).charCodeAt(0)] >
+          hash_t[s.charAt(start).charCodeAt(0)] ||
+        hash_t[s.charAt(start).charCodeAt(0)] == 0
+      ) {
+        let ac = s.charAt(start).charCodeAt(0);
+        if (hash_s[ac] > hash_t[ac]) {
+          hash_s[ac]--;
+        }
+        start++;
+      }
+      //update window size
+      var len_win = j - start + 1;
+      if (min_len > len_win) {
+        min_len = len_win;
+        start_index = start;
+      }
+    }
+  }
+  return s.substring(start_index, start_index + min_len);
+}
+
+
 let result = findSub(str1, str2);
 prt(result);
